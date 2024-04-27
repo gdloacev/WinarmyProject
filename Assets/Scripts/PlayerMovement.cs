605038bf7bed5ab5c6efd6aa444528da162d5124
Oscar Aceves <oaceves@classvaluation.com>
2024-04-27 02:50:25 -0600

    Sounds

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -10,7 +10,9 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     private Rigidbody _rb = null;
-    private int _groundLayerMask = 0;  
+    private int _groundLayerMask = 0;
+    private float horizontal = 0f;
+    private float vertical = 0f;
 
     private void Awake() { 
         _rb = GetComponent<Rigidbody>();
@@ -19,13 +21,16 @@ public class PlayerMovement : MonoBehaviour
     private void Update()
     {
         if (GameSceneManager.Instance.GameState != GameState.Playing) return;
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+    }
 
+    private void FixedUpdate()
+    {
         if (_allowMovement)
         {
-            var horizontal = Input.GetAxis("Horizontal");
-            var vertical = Input.GetAxis("Vertical");
             Move(horizontal, vertical);
-            if (Input.GetButtonDown("Fire1") && IsGrounded()) transform.position += _jumpSpeed * Time.deltaTime * Vector3.up;
+            if (Input.GetButtonDown("Fire1") && IsGrounded()) transform.position += _jumpSpeed * Time.fixedDeltaTime * Vector3.up;
         }
         else
         {
