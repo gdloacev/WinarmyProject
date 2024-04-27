d1a201aaed94e032fa193831b423b12d875e52c6
Oscar Aceves <oaceves@classvaluation.com>
2024-04-26 21:09:30 -0600

    Movement and Acoplation

--- Assets/Scripts/PlayersAcoplation.cs
+++ Assets/Scripts/PlayersAcoplation.cs
@@ -4,29 +4,35 @@ using UnityEngine;
 
 public class PlayersAcoplation : MonoBehaviour
 {
-    [SerializeField] private Transform _acoplationPoint = null;
+    private Transform _acoplationPoint = null;
+    private PlayerMovement _playerMovement;
 
-    private PlayerMovement _playerMovement = null;
-    private Rigidbody _rb;
-    
-
-    private void Awake() { 
-        _playerMovement = GetComponent<PlayerMovement>();
-        _rb = GetComponent<Rigidbody>();        
+    private void Awake()
+    {
+       _playerMovement = GetComponent<PlayerMovement>();
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision != null && this.CompareTag("Mouse") && collision.gameObject.CompareTag("Dino") && _playerMovement.IsMovementAllowed()) {
-            PlayerAcoplation(collision.gameObject);
+        if (collision != null && collision.gameObject.CompareTag("Dino") && _playerMovement.IsMovementAllowed() && (this.CompareTag("Mouse") || this.CompareTag("Dino"))) {
+            var source = this.CompareTag("Mouse") ? this.gameObject : this.gameObject.transform.GetChild(2).gameObject;
+            var hasParent = this.CompareTag("Dino");
+            PlayerAcoplation(source, collision.gameObject, hasParent);
         }
     }
 
-    private void PlayerAcoplation(GameObject target) {
-        transform.position = _acoplationPoint.position;
-        transform.parent = _acoplationPoint.parent;
-        _rb.constraints = RigidbodyConstraints.FreezeAll;
-        _playerMovement.SetAllowMovement(false);
-        target.GetComponent<PlayerMovement>().SetAllowMovement(true);
+    private void PlayerAcoplation(GameObject source, GameObject target, bool hasParent) {
+        _acoplationPoint = target.transform.GetChild(1);
+        var parent = (hasParent) ? source.transform.parent : null;
+        if (parent != null)
+        {
+            parent.GetComponent<PlayerMovement>().SetAllowMovement(false);
+            parent.tag = string.Empty;
+        }
+        source.transform.position = _acoplationPoint.position;
+        source.transform.SetParent(_acoplationPoint.parent);
+        source.GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeAll;
+        source.GetComponent<PlayerMovement>().SetAllowMovement(false);
+        target.GetComponent<PlayerMovement>().SetAllowMovement(true);        
     }
 }
