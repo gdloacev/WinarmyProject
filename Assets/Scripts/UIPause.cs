98ab72aaebda4d9ea8e5dfb80d5e2b4c124a9fee
Alfredo Zum <alfredo.conomia@gmail.com>
2024-04-26 21:49:19 -0600

    MEnu de pausa

---
+++ Assets/Scripts/UIPause.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using TMPro;
+using UnityEngine.UI;
+
+public class UIPause : MonoBehaviour
+{
+    private bool isPaused = false;
+    [SerializeField] GameObject menuPause;
+    // public TMP_Button buttonToSelect;
+    public Button buttonToSelect;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            EventPause();
+        }
+    }
+
+    public void EventPause()
+    {
+
+        isPaused = !isPaused;
+
+        menuPause.SetActive(isPaused);
+
+        if (isPaused)
+        {
+            Time.timeScale = 0f;
+            EventSystem.current.SetSelectedGameObject(buttonToSelect.gameObject);
+        }
+        else
+        {
+            Time.timeScale = 1f;
+            EventSystem.current.SetSelectedGameObject(null);
+        }
+    }
+
+    public void GoToStartingScreen()
+    {
+        SceneManager.LoadScene("Inicio");
+    }
+}
